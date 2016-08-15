28981013083e5e5f2eb16542569d56787feaaf7a
merrillrudd <merrillrudd@gmail.com>
2016-08-15 16:40:36 -0700

    add calc_AIC function

--- R/LIME_functions.R
+++ R/LIME_functions.R
@@ -2427,3 +2427,36 @@ spatialgrowth_sim <- function(n_i, Scale=2, Sigma2=1, SD_spatial=0.1, linf, beta
     df <- data.frame(linf_i=linf_i, y_i=y_i)
     return(df)
 }
+
+#' Calculate AIC
+#'
+#' \code{calc_AIC} calculate AIC for model runs
+#'
+#' @param modpath_vec path to model run
+
+#' @return matrix with AIC, AICc, deltaAIC, and deltaAICc (in that order by column) for each model (down the rows)
+#' @export
+calc_AIC <- function(modpath_vec){
+    aic_mat <- matrix(NA, nrow(length(modpath_vec)), ncol=4)
+    colnames(aic_mat) <- c("AIC", "AICc", "deltaAIC", "deltaAICc")
+
+    for(i in 1:length(modpath_vec)){
+        input <- readRDS(file.path(modpath_vec[i], "Inputs2.rds"))
+        report <- readRDS(file.path(modpath_vec[i], "Report.rds"))
+
+        nll <- report$jnll
+        params <- length(as.vector(unlist(input$Parameters))) - length(which(grepl("Nu_input", names(unlist(input$Parameters)))))
+        sampsize <- length(which(input$Data$I_t>0)) + sum(input$Data$obs_per_yr) + length(which(input$Data$C_t>0))
+
+        AIC <- 2*params + 2*nll
+        AICc <- AIC + (2*params*(params + 1))/(sampsize - params - 1)
+
+        aic_mat[i,1] <- AIC
+        aic_mat[i,2] <- AICc
+    }
+
+    aic_mat[,"deltaAIC"] <- sapply(1:nrow(aic_mat), function(x) exp((min(aic_mat[,"AIC"]) -aic_mat[x,"AIC"])/2))
+    aic_mat[,"deltaAICc"] <- sapply(1:nrow(aic_mat), function(x) exp((min(aic_mat[,"AICc"]) - aic_mat[x,"AICc"])/2))
+
+    return(aic_mat)
+}
