3156e08f5f798566c705d3c4f3215206b080888d
merrillrudd <merrillrudd@gmail.com>
2016-09-02 12:00:31 -0700

    save BBmsy

--- R/LIME_functions.R
+++ R/LIME_functions.R
@@ -1907,6 +1907,7 @@ generateData <- function(modpath, modname, itervec, spatial, Fdynamics, Rdynamic
 
     DataList_out$MSY <- Derived$MSY
     DataList_out$Fmsy <- Derived$Fmsy
+    DataList_out$SBBmsy <- Derived$SBBmsy
 
       if(write==TRUE) saveRDS(DataList_out, file.path(iterpath, "True.rds"))
       if(write==FALSE) return(DataList_out)
