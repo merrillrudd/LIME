7a07aec433565c4d375fe0c296a0630efe1f494d
merrillrudd <merrillrudd@gmail.com>
2016-10-24 16:38:22 -0700

    update starting value for F1 in create_lh_list to avoid errors in estimation

--- R/LIME_functions.R
+++ R/LIME_functions.R
@@ -773,14 +773,14 @@ create_inputs <- function(param, val, lh_list, data_avail_list){
 #' @param h steepness parameter (default = 1)
 #' @param qcoef starting value for catchability coefficient (when index data is available, default = 1e-5)
 #' @param M value for natural mortality if there has been a study (default = NULL, calculated internally from vbk)
-#' @param F1 starting value for initial fishing mortality (default = 0, will be estimated)
+#' @param F1 starting value for initial fishing mortality. Default = 0.2, do not start at zero because this is used to set the initial values for estimating annual fishing mortality in log space, thus setting to zero would cause an error. 
 #' @param Fequil equilibrium fishing mortality rate (used for simulation; default=0.25)
 #' @param Frate parameter used to simulate fishing moratality time series (default=0.2)
 #' @param Fmax maximum F used in simulation (default=0.7)
 #' @param start_ages age to start (usually either 0 or 1; default = 0)
 #' @return List, a tagged list of life history traits
 #' @export
-create_lh_list <- function(vbk, linf, lwa, lwb, S50, M50, selex_input="length", maturity_input="length", binwidth=1, t0=-0.01, CVlen=0.2, SigmaC=0.2, SigmaI=0.2, SigmaR=0.6, SigmaF=0.3, R0=1,  h=1, qcoef=1e-5, M=NULL, F1=0, Fequil=0.25, Frate=0.2, Fmax=0.7, start_ages=0){
+create_lh_list <- function(vbk, linf, lwa, lwb, S50, M50, selex_input="length", maturity_input="length", binwidth=1, t0=-0.01, CVlen=0.2, SigmaC=0.2, SigmaI=0.2, SigmaR=0.6, SigmaF=0.3, R0=1,  h=1, qcoef=1e-5, M=NULL, F1=0.2, Fequil=0.25, Frate=0.2, Fmax=0.7, start_ages=0){
             
     ## mortality
     if(is.null(M)) M <- 1.5*vbk  ## based on vbk if not specified 
