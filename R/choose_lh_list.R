692fef4194e8b1b035908f8eda1ed69bba5951dc
merrillrudd <merrillrudd@gmail.com>
2016-11-04 09:11:30 -0700

    change hake ML50 to 47 based on fishbase

--- R/choose_lh_list.R
+++ R/choose_lh_list.R
@@ -291,7 +291,7 @@ choose_lh_list <- function(species, selex, param_adjust=FALSE, val=FALSE, start_
         binwidth <- 1
 
         ## selectivity
-        ML50 <- 70
+        ML50 <- 47.4
         S50 <- 2
         SL50 <- round(t0-log(1-(2/linf))/vbk)
 
