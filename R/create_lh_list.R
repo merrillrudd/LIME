ecfb9c2f3f3d1e7404197390336ef3f3df58b04f
merrillrudd <merrillrudd@gmail.com>
2016-12-14 10:22:33 -0800

    add rho AR parameter

--- R/create_lh_list.R
+++ R/create_lh_list.R
@@ -26,9 +26,10 @@
 #' @param Frate parameter used to simulate fishing moratality time series (default=NULL)
 #' @param Fmax maximum F used in simulation (default=NULL)
 #' @param start_ages age to start (either 0 or 1; default = 0)
+#' @param rho first-order autocorrelation in recruitment residuals parameter, default=0 (recruitment not autocorrelated)
 #' @return List, a tagged list of life history traits
 #' @export
-create_lh_list <- function(vbk, linf, lwa, lwb, S50, M50, selex_input="length", maturity_input="length", binwidth=1, t0=-0.01, CVlen=0.1, SigmaC=0.2, SigmaI=0.2, SigmaR=0.6, SigmaF=0.3, R0=1,  h=1, qcoef=1e-5, M=NULL, F1=0.2, Fequil=0.2, Frate=0.2, Fmax=0.7, start_ages=0){
+create_lh_list <- function(vbk, linf, lwa, lwb, S50, M50, selex_input="length", maturity_input="length", binwidth=1, t0=-0.01, CVlen=0.1, SigmaC=0.2, SigmaI=0.2, SigmaR=0.6, SigmaF=0.3, R0=1,  h=1, qcoef=1e-5, M=NULL, F1=0.2, Fequil=0.2, Frate=0.2, Fmax=0.7, start_ages=0, rho=0){
             
     ## mortality
     if(is.null(M)) M <- 1.5*vbk  ## based on vbk if not specified 
@@ -126,5 +127,6 @@ create_lh_list <- function(vbk, linf, lwa, lwb, S50, M50, selex_input="length", 
     Outs$Fequil <- Fequil
     Outs$Frate <- Frate
     Outs$Fmax <- Fmax
+    Outs$rho <- rho
     return(Outs)
 }
