eb58ddd527b076e3dbf5373891099799410b90fc
merrillrudd <merrillrudd@gmail.com>
2017-02-04 18:13:48 -0800

    no double specification of Sel0

--- R/create_lh_list.R
+++ R/create_lh_list.R
@@ -167,7 +167,6 @@ create_lh_list <- function(vbk, linf, lwa, lwb, S50, M50, selex_input="length", 
     Outs$AgeMax <- AgeMax
     Outs$ages <- ages
     Outs$Mat0 <- Mat0
-    Outs$Sel0 <- Sel0
     Outs$mids <- mids
     Outs$highs <- highs
     Outs$lows <- lows
