44c95f5cfd27bd2e46a276600143577fe10e968d
DanOvando <dovando@bren.ucsb.edu>
2017-06-30 08:42:47 -0700

    add in capacity to sample lengths from the population instead of just catch, reformat some of the code

--- R/AgeToLengthComp.R
+++ R/AgeToLengthComp.R
@@ -6,50 +6,61 @@
 #' @param tyears number of years of data
 #' @param N_at matrix of numbers in the population at each age over time
 #' @param comp_sample vector of number of individuals sampled each year (set as 1 for proportions)
+#' @param sample_type a character vector specifying if the length comps are sampled from the 'catch' (default) or from the population
 #'
 #' @return data frame - number of individuals in each length bin in each year
 #' @export
-AgeToLengthComp <- function(lh, tyears, N_at, comp_sample){
+AgeToLengthComp <-
+  function(lh,
+           tyears,
+           N_at,
+           comp_sample,
+           sample_type = 'catch') {
+    with(lh, {
+      ################################################
+      ## Probability being in a length bin given age
+      ################################################
+      lbprobs <-
+        function(mnl, sdl)
+          return(pnorm(highs, mnl, sdl) - pnorm(lows, mnl, sdl))
+      vlprobs <- Vectorize(lbprobs, vectorize.args = c("mnl", "sdl"))
+      plba <- t(vlprobs(L_a, L_a * CVlen))
+      plba <- plba / rowSums(plba)
 
-    with(lh,{
-	################################################
-	## Probability being in a length bin given age
-	################################################
-    lbprobs <- function(mnl,sdl) return(pnorm(highs,mnl,sdl)-pnorm(lows,mnl,sdl))
-    vlprobs <- Vectorize(lbprobs,vectorize.args=c("mnl","sdl"))
-    plba <- t(vlprobs(L_a,L_a*CVlen))
-    plba <- plba/rowSums(plba)
+      ################################################
+      ## Probability being in harvested at an age
+      ################################################
+      page <- matrix(ncol = dim(plba)[1], nrow = tyears)
+      for (y in 1:tyears)
+        page[y, ] <- N_at[, y] * ifelse(sample_type == 'catch', S_a, 1)
+      page <- page / rowSums(page)
 
-    ################################################
-	## Probability being in harvested at an age
-	################################################
-    page <- matrix(ncol=dim(plba)[1], nrow=tyears)
-    for (y in 1:tyears) page[y,] <- N_at[,y] * S_a
-    page <- page/rowSums(page)    
+      ################################################
+      ## Probability of sampling a given length bin
+      ################################################
+      plb <- matrix(ncol = length(highs), nrow = tyears)
+      for (y in 1:tyears)
+        plb[y, ] <- page[y, ] %*% plba
+      plb <- plb / rowSums(plb)
 
-    ################################################
-	## Probability of sampling a given length bin
-	################################################
-    plb <- matrix(ncol=length(highs), nrow=tyears)
-    for (y in 1:tyears) plb[y,] <- page[y,] %*% plba
-    plb <- plb/rowSums(plb)    
-
-    #######################
-	## Length frequencies 
-	#######################
-    LF <- array(0, dim=dim(plb))
+      #######################
+      ## Length frequencies
+      #######################
+      LF <- array(0, dim = dim(plb))
       rownames(LF) <- 1:tyears
-    for(y in 1:tyears){
-    	LF[y,] <- rmultinom(n=1, size=comp_sample[y], prob=plb[y,])
-    }
+      for (y in 1:tyears) {
+        LF[y, ] <- rmultinom(n = 1,
+                             size = comp_sample[y],
+                             prob = plb[y, ])
+      }
 
-    Outs <- NULL
-    Outs$plba <- plba
-    Outs$plb <- plb
-    Outs$page <- page
-    Outs$LF <- LF
-    return(Outs)
-})
+      Outs <- NULL
+      Outs$plba <- plba
+      Outs$plb <- plb
+      Outs$page <- page
+      Outs$LF <- LF
+      return(Outs)
+    })
 
-    
-}
+
+  }
