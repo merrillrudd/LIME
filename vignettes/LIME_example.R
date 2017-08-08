e2309b962ba1ee66128063f90e2f76c702838b1f
merrillrudd <merrillrudd@gmail.com>
2017-08-08 08:07:51 -0700

    include C_opt argument in run_LIME to estimate population scale

--- vignettes/LIME_example.R
+++ vignettes/LIME_example.R
@@ -35,13 +35,19 @@ lh <- create_lh_list(vbk=0.21,
 					 rho=0.43,
 					 nseasons=1)
 
+par(mfrow=c(2,2))
+plot(lh$L_a, type="l", lwd=3, col="forestgreen", xlab="Age", ylab="Length")
+plot(lh$W_a, type="l", lwd=3, col="forestgreen", xlab="Age", ylab="Weight")
+plot(lh$Mat_l, type="l", lwd=3, col="forestgreen", xlab="Length", ylab="Proportion mature")
+plot(lh$S_l, type="l", lwd=3, col="forestgreen", xlab="Length", ylab="Proportion vulnerable to gear")
+
 ##----------------------------------------------------
 ## Step 2: Setup data input
 ## ---------------------------------------------------
 set.seed(143)
 ## Demonstrate data generation option
 true <- generate_data(modpath=NULL,
-					  data_avail="LC",
+					  data_avail="Index_Catch_LC",
 					  itervec=1, 
 					  Fdynamics="Ramp",
 					  Rdynamics="AR",
@@ -49,7 +55,7 @@ true <- generate_data(modpath=NULL,
 					  Nyears=20,
 					  Nyears_comp=10,
 					  comp_sample=200,
-					  init_depl=0.4)
+					  init_depl=0.8)
 
 ## Data input components
 years <- true$years ## total years to model, can be 1:20 or 1998:2017
@@ -114,26 +120,30 @@ plot_output(all_years=1:20,
 			lh=lh, 
 			true_years=1998:2017, 
 			True=true, 
-			plot=c("Fish","Rec","SPR","ML","SB","Selex"))
+			plot=c("Fish","Rec","SPR","Selex"))
 
 
 
 
-##----------------------------------------------------
-## Step 5: Run with other data types
-## ---------------------------------------------------
-C_t <- true$C_t ## (optional) catch data, with elements of vector named with the year observed, e.g. 1:20 or 1998:2017 (can be any years within years to model)
+##----------------------------------------------------------------------------
+## Step 5: Run with other data types - catch and abundance index examples
+## ---------------------------------------------------------------------------
+Cw_t <- true$Cw_t ## (optional) catch data, with elements of vector named with the year observed, e.g. 1:20 or 1998:2017 (can be any years within years to model)
 I_t <- true$I_t ## (optional) abundance index, with elements of vector named with the year observed, e.g. 1:20 or 1998:2017 (can be any years within years to model)
 
-data_LF_Catch <- list("years"=years, "LF"=LF, "C_t"=C_t) ## length comp + catch
+data_LF_Catch <- list("years"=years, "LF"=LF, "C_t"=Cw_t) ## length comp + catch
 data_LF_Index <- list("years"=years, "LF"=LF, "I_t"=I_t) ## length comp + index
+data_rich <- list("years"=years, "LF"=LF, "I_t"=I_t, "C_t"=Cw_t) ## length comp + index
 
-## length comp + catch
+
+## length comp + index + catch
+## add option to specify catch in biomass (2) or numbers (1)
 res <- run_LIME(modpath=NULL,
 				lh=lh,
-				input_data=data_LF_Catch,
+				input_data=data_rich,
 				est_sigma="log_sigma_R", 
-				data_avail="Catch_LC")
+				data_avail="Index_Catch_LC",
+				C_opt=2) 
 
 ## check convergence
 check <- res$df
@@ -165,7 +175,49 @@ plot_output(all_years=1:20,
 			lh=lh, 
 			true_years=1998:2017, 
 			True=true, 
-			plot=c("Fish","Rec","SPR","ML","SB","Selex"))
+			plot=c("Fish","Rec","SPR","Selex"))
+
+
+## length comp + catch
+## add option to specify catch in biomass (2) or numbers (1)
+res <- run_LIME(modpath=NULL,
+				lh=lh,
+				input_data=data_LF_Catch,
+				est_sigma="log_sigma_R", 
+				data_avail="Catch_LC",
+				C_opt=2)
+
+## check convergence
+check <- res$df
+## check for other issues
+issues <- res$issues
+## check TMB inputs
+Inputs <- res$Inputs
+## Report file
+Report <- res$Report
+## Standard error report
+Sdreport <- res$Sdreport
+
+## plot length composition data
+plot_LCfits(Inputs=Inputs$Data, 
+			Report=Report,
+			true_lc_years=2008:2017, 
+			ylim=NULL, 
+			ML50=lh$ML50, 
+			SL50=Report$S50,
+			dim=c(5,2), 
+			n=FALSE)
+
+## plot model output
+plot_output(all_years=1:20,
+			lc_years=11:20, 
+			Inputs=Inputs, 
+			Report=Report, 
+			Sdreport=Sdreport, 
+			lh=lh, 
+			true_years=1998:2017, 
+			True=true, 
+			plot=c("Fish","Rec","SPR","Selex"))
 
 
 ## length comp + index
@@ -205,4 +257,4 @@ plot_output(all_years=1:20,
 			lh=lh, 
 			true_years=1998:2017, 
 			True=true, 
-			plot=c("Fish","Rec","SPR","ML","SB","Selex"))
+			plot=c("Fish","Rec","SPR","Selex"))
