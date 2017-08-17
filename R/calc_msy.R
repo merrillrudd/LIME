bc33c068207fd95e26afc7b82fd6a00dfa101b78
merrillrudd <merrillrudd@gmail.com>
2017-08-16 21:53:17 -0700

    add function to calculate MSY

--- R/calc_msy.R
+++ R/calc_msy.R
@@ -1,3 +1,17 @@
+#' Calculate MSY reference point
+#'
+#' \code{calc_msy} calculates yield per recruit, when maximized across levels of F produces Fmsy
+#'
+#' @param F fishing mortality rate
+#' @param ages ages in the population
+#' @param S_a selectivity-at-age
+#' @param M natural mortality
+#' @param R0 equilibrium recruits
+#' @param W_a weight-at-age
+#' @param Mat_a maturity-at-age
+
+#' @return yield per recruit, in biomass
+#' @export
 calc_msy <- function(F, ages, S_a, M, R0, W_a, Mat_a){
 
 	Nage <- calc_equil_abund(ages=ages, S_a=S_a, M=M, F=F, R0=R0)
