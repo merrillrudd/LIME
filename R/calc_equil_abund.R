bbb233c5dacdf8f196d7ab7fcb7b647abdbad48d
merrillrudd <merrillrudd@gmail.com>
2018-01-30 16:57:23 -0800

    remove selectivity and calculate total F

--- R/calc_equil_abund.R
+++ R/calc_equil_abund.R
@@ -4,7 +4,6 @@
 #'
 #' @author M.B. Rudd
 #' @param ages vector of ages
-#' @param S_a selectivity-at-age
 #' @param M natural mortality
 #' @param F fishing mortality
 #' @param R0 recruitment
@@ -12,12 +11,13 @@
 #' @return vector of expected abundance at age
 #' @export
 
-calc_equil_abund <- function(ages, S_a, M, F, R0){
+calc_equil_abund <- function(ages, M, F, R0){
 	N_a <- rep(NA, length(ages))
 	N_a[1] <- R0
+
 	for(i in 2:length(ages)){
-		if(i<length(ages)) N_a[i] <- N_a[i-1]*exp(-M-S_a[i-1]*F)
-		if(i==length(ages)) N_a[i] <- (N_a[i-1]*exp(-M-S_a[i-1]*F))/(1-exp(-M-S_a[i-1]*F))
+		if(i<length(ages)) N_a[i] <- N_a[i-1]*exp(-M-F)
+		if(i==length(ages)) N_a[i] <- (N_a[i-1]*exp(-M-F))/(1-exp(-M-F))
 	}
 	return(N_a)
 }
