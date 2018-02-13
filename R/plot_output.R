4f69485f60848d19a4bf780721ae1ce086f2b04e
merrillrudd <merrillrudd@gmail.com>
2018-02-12 18:58:07 -0800

    rework for multifleet

--- R/plot_output.R
+++ R/plot_output.R
@@ -20,8 +20,15 @@
 #' @export
 plot_output <- function(Inputs=NULL, Report=NULL, Sdreport=NULL, LBSPR=NULL, lh, true_years=NULL, True=NULL, plot=c("Fish","Rec","SPR","ML","SB","Selex"), set_ylim=list("Fish" = c(0,1), "SPR" = c(0,1))){
 
+    nf <- Inputs$Data$n_f
+    # nf <- 1
+    ns <- Inputs$Data$n_s
+
   all_years <- 1:Inputs$Data$n_t
-  lc_years <- all_years
+  lc_years <- lapply(1:nf, function(x){
+    sub <- Inputs$Data$LF_tlf[,,x]
+    return(which(rowSums(sub) > 0))
+  })
   if(all(is.null(true_years))) true_years <- all_years
 
     if(is.null(LBSPR)==FALSE){
@@ -52,18 +59,14 @@ if(length(plot)==5 | length(plot)==6) dim <- c(2,3)
     lab <- rev(seq(from=true_years[length(true_years)], to=min(true_years), by=-by))
     ilab <- which(true_years %in% lab)
 
-    # nf <- Inputs$Data$n_f
-    nf <- 1
-    ns <- Inputs$Data$n_s
-
 if(all(is.null(Inputs))==FALSE){
   if(ns==1){
     xY <- seq_along(all_years)
-    xLC <- which(all_years %in% lc_years)
+    xLC <- lapply(1:nf, function(x) which(all_years %in% lc_years[[x]]))
   }
   if(ns>1){
     xY <- 1:Inputs$Data$n_y
-    xLC <- unique(Inputs$Data$S_yrs[which(all_years %in% lc_years)])
+    xLC <- lapply(1:nf, function(x) unique(Inputs$Data$S_yrs[which(all_years %in% lc_years[[x]])]))
   }
   ilab2 <- sapply(1:length(ilab), function(x){
     sub <- which(Inputs$Data$S_yrs %in% ilab[x])
@@ -73,7 +76,7 @@ if(all(is.null(Inputs))==FALSE){
 if(all(is.null(Inputs))){
   if(all(is.null(LBSPR))) stop("Must specify either LIME or LBSPR output")
   xY <- seq_along(all_years)
-  xLC <- which(all_years %in% lc_years)
+  xLC <- lapply(1:nf, function(x) which(all_years %in% lc_years[[x]]))
   ilab2 <- sapply(1:length(ilab), function(x){
     sub <- which(seq_along(all_years) %in% ilab[x])
     return(sub[length(sub)])
@@ -83,9 +86,16 @@ if(all(is.null(Inputs))){
 par(mfrow=dim, mar=c(4,5,2,2))
 
 if(all(is.null(Inputs))==FALSE){
-  F40 <- tryCatch(sapply(1:nf, function(x) uniroot(calc_ref, lower=0, upper=200, ages=lh$ages, Mat_a=Report$Mat_a, W_a=Report$W_a, M=Report$M, S_a=Report$S_fa[x,], ref=0.4)$root), error=function(e) NA)
-  F30 <- tryCatch(sapply(1:nf, function(x) uniroot(calc_ref, lower=0, upper=200, ages=lh$ages, Mat_a=Report$Mat_a, W_a=Report$W_a, M=Report$M, S_a=Report$S_fa[x,], ref=0.3)$root), error=function(e) NA)
+  F40 <- tryCatch(uniroot(calc_ref, lower=0, upper=200, ages=lh$ages, Mat_a=Report$Mat_a, W_a=Report$W_a, M=Report$M, ref=0.4)$root, error=function(e) NA)
+  F30 <- tryCatch(uniroot(calc_ref, lower=0, upper=200, ages=lh$ages, Mat_a=Report$Mat_a, W_a=Report$W_a, M=Report$M, ref=0.3)$root, error=function(e) NA)
 }
+    col_total <- "#228B22"
+    if(nf>1){
+      colfn <- colorRampPalette(c("red","blue"))
+      cols <- colfn(nf)
+    }
+    if(nf==1) cols <- col_total
+
 
 if("Fish" %in% plot){
     if("Fish" %in% names(set_ylim) ==FALSE) ylim <- c(0, max(Report$F_y)*2)
@@ -93,38 +103,34 @@ if("Fish" %in% plot){
 
   if(all(is.null(Sdreport))==FALSE){
     if(all(is.na(Sdreport))==FALSE){
-      sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="lF_y"),]
-      sd[,2][which(is.na(sd[,2]))] <- 0
+      sd_total <- summary(Sdreport)[which(rownames(summary(Sdreport))=="F_y"),]
+      sd_fleet <- summary(Sdreport)[which(rownames(summary(Sdreport))=="F_fy")]
+      if(is.vector(sd_fleet)) sd_fleet <- as.matrix(sd_fleet)
+      # sd[,2][which(is.na(sd[,2]))] <- 0
       # ylim <- c(0, max(max(read_sdreport(sd, log=TRUE))*1.2))#, ymax))
     }
   }
 
   if(all(is.null(Report))==FALSE){
-    if(nf>1){
-      colfn <- colorRampPalette(c("red","blue"))
-      cols <- colfn(nf)
-    }
-    if(nf==1) cols <- "#228B22"
-    for(f in 1:nf){
-      if(f==1) plot(x=xY, y=Report$F_y, lwd=2, col=cols[f], ylim=ylim, type="l", xaxt="n", xaxs="i", yaxs="i", cex.axis=2, ylab="Fishing mortality", xlab="Year", cex.lab=2, xlim=c(min(xY),max(xY)))
-      if(f>1) lines(x=xY, y=Report$F_y, lwd=2, col=cols[f])
-      points(x=xLC, y=Report$F_y[xLC], col=cols[f], pch=19, cex=2, xpd=NA)
-      index <- seq(f, nrow(sd), by=nf)
+    plot(x=1, y=1, type="n", xaxt="n", xaxs="i", yaxs="i", ylim=ylim, cex.axis=2, ylab="Fishing mortality", xlab="Year", cex.lab=2, xlim=c(min(xY),max(xY)))
+
+      # index <- seq(f, nrow(sd), by=nf)
       if(all(is.na(Sdreport))==FALSE){
-        polygon( y=read_sdreport(sd[index,], log=TRUE), x=c(which(is.na(sd[index,2])==FALSE), rev(which(is.na(sd[index,2])==FALSE))), col=paste0(cols[f],"20"), border=NA)  
+        polygon( y=read_sdreport(sd_total, log=FALSE), x=c(which(is.na(sd_total[,2])==FALSE), rev(which(is.na(sd_total[,2])==FALSE))), col=paste0(col_total,"40"), border=NA)  
       }
+
+    for(f in 1:nf){
+      lines(x=xY, y=sd_fleet[seq(from=f, to=nrow(sd_fleet), by=nf),1], lwd=2, col=cols[f])
+      points(x=xLC[[f]], y=sd_fleet[seq(from=f, to=nrow(sd_fleet), by=nf)[xLC[[f]]],1], col=cols[f], pch=19, cex=2, xpd=NA)
     }
+    lines(x=xY, y=sd_total[,1], lwd=2, col=col_total)
+    points(x=unique(unlist(xLC)), y=sd_total[unique(unlist(xLC)),1], lwd=2, col=col_total, pch=19, cex=2, xpd=NA)
   }
 
-  if(all(is.null(Inputs))==FALSE){
-    if(nf>1){
-      colfn <- colorRampPalette(c("red","blue"))
-      cols <- colfn(nf)
-    }
-    if(nf==1) cols <- "#228B22"    
+  if(all(is.null(Inputs))==FALSE){  
     makelines <- sapply(1:nf, function(x){
-      abline(h=F40[x]*ns, lwd=2, lty=2, col=cols[x])
-      abline(h=F30[x]*ns, lwd=2, lty=3, col=cols[x])
+      abline(h=F40[x]*ns, lwd=2, lty=2)
+      abline(h=F30[x]*ns, lwd=2, lty=3)
     })
   }
 
@@ -150,35 +156,38 @@ if("Fish" %in% plot){
 }
 
 if("Rec" %in% plot){
+
   if(all(is.na(Sdreport))==FALSE){
       sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="lR_t"),]
       sd[,2][which(is.na(sd[,2]))] <- 0
-  }
-  if("Rec" %in% names(set_ylim) == FALSE) ylim <- c(0, max(read_sdreport(sd, log=TRUE)))
-  if("Rec" %in% names(set_ylim)) ylim <- set_ylim[["Rec"]]
+      r_est <- exp(sd[,1])
 
-  plot(x=seq_along(all_years), y=Report$R_t, lwd=2, col="#228B22", ylim=c(0, max(Report$R_t)*1.5), type="l", xaxt="n", ylab="Recruitment", xlab="Year", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2, xlim=c(min(seq_along(all_years)),max(seq_along(all_years))), xpd=NA)
-  points(x=which(all_years %in% lc_years), y=Report$R_t[which(all_years %in% lc_years)], col="#228B22", pch=19, cex=2, xpd=NA)
-  if(all(is.na(Sdreport))==FALSE){
-    polygon( y=read_sdreport(sd, log=TRUE), x=c(which(is.na(sd[,2])==FALSE), rev(which(is.na(sd[,2])==FALSE))), col="#228B2220", border=NA)
+      if("Rec" %in% names(set_ylim) == FALSE) ylim <- c(0, max(read_sdreport(sd, log=TRUE)))
+      if("Rec" %in% names(set_ylim)) ylim <- set_ylim[["Rec"]]
+      
+      plot(x=1, y=1, type="n", xaxt="n", xaxs="i", yaxs="i", ylim=ylim, cex.axis=2, ylab="Recruitment", xlab="Year", cex.lab=2, xlim=c(min(xY),max(xY)))    
+        polygon( y=read_sdreport(sd, log=TRUE), x=c(which(is.na(sd[,2])==FALSE), rev(which(is.na(sd[,2])==FALSE))), col=paste0(col_total, "40"), border=NA)
+        lines(x=seq_along(all_years), y=r_est, lwd=2, col=col_total, ylim=ylim, xpd=NA)
+        points(x=unique(unlist(xLC)), y=r_est[unique(unlist(xLC))], col=col_total, pch=19, cex=2, xpd=NA)
+        axis(1, cex.axis=2, at=ilab2, labels=lab)
+        if(all(is.null(True))==FALSE) lines(True$R_t, lwd=2)
   }
-  axis(1, cex.axis=2, at=ilab2, labels=lab)
-  if(all(is.null(True))==FALSE) lines(True$R_t, lwd=2)
 
 }
 
 if("SPR" %in% plot){
-  if(all(is.null(Report))==FALSE){
+
   if("SPR" %in% names(set_ylim) == FALSE) ylim <- c(0,1)
   if("SPR" %in% names(set_ylim)) ylim <- set_ylim[["SPR"]]
-    plot(x=seq_along(all_years), y=Report$SPR_t, lwd=2, col="#228B22", ylim=c(0, 1), type="l", xaxt="n", ylab="SPR", xlab="Year", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2, xlim=c(min(seq_along(all_years)),max(seq_along(all_years))), xpd=NA)
-    points(x=which(all_years %in% lc_years), y=Report$SPR_t[which(all_years%in%lc_years)], col="#228B22", pch=19, cex=2, xpd=NA)
-  }
+  plot(x=1, y=1, type="n", xaxt="n", ylab="SPR", xlab="Year", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2, xlim=c(min(seq_along(all_years)), max(seq_along(all_years))), ylim=ylim)
+
   if(all(is.null(Sdreport))==FALSE){
     if(all(is.na(Sdreport))==FALSE){
-    sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="SPR_t"),]
-    sd[,2][which(is.na(sd[,2]))] <- 0
-    polygon( y=read_sdreport(sd, log=FALSE), x=c(which(is.na(sd[,2])==FALSE), rev(which(is.na(sd[,2])==FALSE))), col="#228B2220", border=NA)
+      sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="SPR_t"),]
+      sd[,2][which(is.na(sd[,2]))] <- 0
+      polygon( y=read_sdreport(sd, log=FALSE), x=c(which(is.na(sd[,2])==FALSE), rev(which(is.na(sd[,2])==FALSE))), col=paste0(col_total, "40"), border=NA)
+      lines(x=seq_along(all_years), y=sd[,1], lwd=2, col=col_total)
+      points(x=unique(unlist(xLC)), y=sd[unique(unlist(xLC)),1], pch=19, cex=2, xpd=NA, col=col_total)
     } 
   }
   if(all(is.null(True))==FALSE) lines(True$SPR_t, lwd=2)
@@ -205,48 +214,46 @@ if("SPR" %in% plot){
 
 if("ML" %in% plot){
 
-  if("ML" %in% names(set_ylim) == FALSE) ylim <- c(0, max(Report$L_ft_hat)*1.5)
+  if("ML" %in% names(set_ylim) == FALSE) ylim <- c(0, max(Report$ML_ft_hat)*1.5)
   if("ML" %in% names(set_ylim)) ylim <- set_ylim[["ML"]]
+  
+  plot(x=1,y=1, type="n", xaxt="n", ylab="Mean length", xlab="Year", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2, xlim=c(min(seq_along(all_years)), max(seq_along(all_years))), ylim=ylim)
 
   if(all(is.na(Sdreport))==FALSE){
-    sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="L_ft_hat"),]
+    sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="ML_ft_hat"),]
     sd[,2][which(is.na(sd[,2]))] <- 0
-  }
-    if(nf>1){
-      colfn <- colorRampPalette(c("red","blue"))
-      cols <- colfn(nf)
+    ML_obs <- lapply(1:nf, function(y){
+      subLF <- Inputs$Data$LF_tlf[,,y]
+      sapply(1:nrow(subLF), function(x){
+        sum(subLF[x,]*Inputs$Data$lbmids)/sum(subLF[x,])
+      })
+    })
+    for(f in 1:nf){
+      index <- seq(f,nrow(sd),by=nf)
+      polygon(y=read_sdreport(sd[index,], log=FALSE), x=c(which(is.na(sd[index,2])==FALSE), rev(which(is.na(sd[index,2])==FALSE))), col=paste0(cols[f],"40"), border=NA)
+      lines(x=seq_along(all_years), y=sd[index,1], lwd=2, col=cols[f])
+      points(x=unique(unlist(xLC)), y=sd[index[unique(unlist(xLC))],1], pch=19, col=cols[f], xpd=NA, cex=2)
+      lines(x=unique(unlist(xLC)), y=ML_obs[[f]], lwd=2, xpd=NA)
     }
-    if(nf==1) cols <- "#228B22"
-  for(f in 1:nf){
-    ML_obs <- sapply(1:nrow(Inputs$Data$LF), function(x) sum(Inputs$Data$LF[x,]*Inputs$Data$lbmids)/sum(Inputs$Data$LF[x,]))
-
-    if(f==1)   plot(x=seq_along(all_years), y=Report$L_ft_hat, lwd=2, col=cols[f], ylim=ylim, type="l", xaxt="n", ylab="Mean length", xlab="Year", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2, xlim=c(min(seq_along(all_years)), max(seq_along(all_years))))
-    if(f>1) lines(x=seq_along(all_years), y=Report$L_ft_hat, lwd=2, col=cols[f])
-    points(x=which(all_years %in% lc_years), y=Report$L_ft_hat[which(all_years %in% lc_years)], col=cols[f], pch=19, cex=2, xpd=NA)
-    points(x=which(all_years %in% lc_years), y=ML_obs, cex=2.5, xpd=NA, col=cols[f], lwd=2)
-    index <- seq(f,nrow(sd), by=nf)
-    polygon( y=read_sdreport(sd[index,], log=FALSE), x=c(which(is.na(sd[index,2])==FALSE), rev(which(is.na(sd[index,2])==FALSE))), col=paste0(cols[f],"20"), border=NA)
   }
-
-
 
   axis(1, cex.axis=2, at=ilab2, labels=lab)
-  if(all(is.null(True))==FALSE) lines(True$ML_t, lwd=2)
 
 }
 
 if("SB" %in% plot){
-  if(all(is.na(Sdreport))==FALSE){
-      sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="lD_t"),]
-      sd[,2][which(is.na(sd[,2]))] <- 0
-  }
   if("SB" %in% names(set_ylim) == FALSE) ylim <- c(0, max(Report$D_t)*1.5)
   if("SB" %in% names(set_ylim)) ylim <- set_ylim[["SB"]]
 
-  plot(x=seq_along(all_years), y=Report$D_t, lwd=2, col="#228B22", ylim=ylim, type="l", xaxt="n", ylab="Relative spawning biomass", xlab="Year", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2, xlim=c(min(seq_along(all_years)), max(seq_along(all_years))+lh$nseasons))
-  points(x=which(all_years %in% lc_years), y=Report$D_t[which(all_years %in% lc_years)], col="#228B22", pch=19, cex=2, xpd=NA)
+  plot(x=1, y=1, type="n", ylab="Relative spawning biomass", xlab="Year", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2, xlim=c(min(seq_along(all_years)), max(seq_along(all_years))), ylim=ylim)
+
   if(all(is.na(Sdreport))==FALSE){
-    polygon( y=read_sdreport(sd, log=TRUE), x=c(which(is.na(sd[,2])==FALSE), rev(which(is.na(sd[,2])==FALSE))), col="#228B2220", border=NA)
+      sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="lD_t"),]
+      sd[,2][which(is.na(sd[,2]))] <- 0
+      polygon(y=read_sdreport(sd, log=TRUE), x=c(which(is.na(sd[,2])==FALSE), rev(which(is.na(sd[,2])==FALSE))), col=paste0(col_total, "40"), border=NA)
+      lines(x=seq_along(all_years), y=exp(sd[,1]), lwd=2, col=col_total)
+      points(x=unique(unlist(xLC)), y=exp(sd[unique(unlist(xLC)),1]), pch=19, col=col_total, xpd=NA, cex=2)
+
   }
   axis(1, cex.axis=2, at=ilab2, labels=lab)
   if(all(is.null(True))==FALSE) lines(True$D_t, lwd=2)
@@ -267,7 +274,7 @@ if("Selex" %in% plot){
 
   if(all(is.null(Sdreport))==FALSE){
     if(all(is.na(Sdreport))==FALSE){
-      sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="S_l"),]
+      sd <- summary(Sdreport)[which(rownames(summary(Sdreport))=="S_fl"),]
       sd[,2][which(is.na(sd[,2]))] <- 0
       # ylim <- c(0, max(max(read_sdreport(sd, log=TRUE))*1.2))#, ymax))
     }
@@ -282,11 +289,14 @@ if("Selex" %in% plot){
     if(nf==1) cols <- "#228B22"
 
     for(f in 1:nf){
-      if(f==1) plot(x=1:length(mids), y=Report$S_l, lwd=2, col=cols[f], ylim=c(0, 1.1), type="l", xaxt="n", ylab="Selectivity at length", xlab="Length (cm)", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2)
-      if(f>1) lines(x=1:length(mids), y=Report$S_l, lwd=2, col=cols[f])
+      if(f==1) plot(x=1:length(mids), y=Report$S_fl[f,], lwd=2, col=cols[f], ylim=c(0, 1.1), type="l", xaxt="n", ylab="Selectivity at length", xlab="Length (cm)", xaxs="i", yaxs="i", cex.axis=2, cex.lab=2)
+      if(f>1) lines(x=1:length(mids), y=Report$S_fl[f,], lwd=2, col=cols[f])
+      if(nf > 1) lty <- f+1
+      if(nf ==1) lty <- 1
+      if(all(is.null(True))==FALSE) lines(True$S_fl[f,], lwd=2, lty=lty)
       if(all(is.na(Sdreport))==FALSE){
         index <- seq(f,nrow(sd),by=nf)
-        polygon( y=read_sdreport(sd[index,], log=FALSE), x=c(which(is.na(sd[index,2])==FALSE), rev(which(is.na(sd[index,2])==FALSE))), col=paste0(cols[f],"20"), border=NA)  
+        polygon( y=read_sdreport(sd[index,], log=FALSE), x=c(which(is.na(sd[index,2])==FALSE), rev(which(is.na(sd[index,2])==FALSE))), col=paste0(cols[f],"40"), border=NA)  
       }
     }
   }
@@ -313,8 +323,8 @@ if("Selex" %in% plot){
     axis(1, cex.axis=2, at=xlabs, labels=plot_labs)
 }
 
-if("Fish" %in% plot | "Selex" %in% plot | "ML" %in% plot & nf > 1){
-  legend("bottomright", col=c("#228B22", cols), legend=c("Total", sapply(1:nf, function(x) paste0("Fleet ", x))), lty=1, lwd=2)
+if(nf > 1){
+  legend("bottomright", col=c("#228B22", cols), legend=c("Total", sapply(1:nf, function(x) paste0("Fleet ", x))), lty=1, lwd=2)  
 }
 
 }
