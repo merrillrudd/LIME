8aad102b677d214008539a74e29d50d9e16a973e
merrillrudd <merrillrudd@gmail.com>
2018-02-26 13:00:26 -0800

    master example with 2cm binwidth'

--- LIME_examples/LIME_example.R
+++ LIME_examples/LIME_example.R
@@ -2,7 +2,7 @@ rm(list=ls())
 
 ## Packages
 
-devtools::install_github("merrillrudd/LIME", dependencies=TRUE, ref="multifleet")
+devtools::install_github("merrillrudd/LIME", dependencies=TRUE, ref="master")
 library(LIME)
 
 devtools::install_github("kaskr/TMB_contrib_R/TMBhelper", dependencies=TRUE)
@@ -16,31 +16,6 @@ library(reshape2)
 ##----------------------------------------------------------------
 ## Step 1: Specify biological inputs and parameter starting values
 ##----------------------------------------------------------------
-# lh <- create_lh_list(vbk=0.21, 
-# 					 linf=65, 
-# 					 t0=-0.01,
-# 					 lwa=0.0245, 
-# 					 lwb=2.79, 
-# 					 S50=c(20,30), 
-# 					 S95=c(26,36), 
-# 					 selex_input="length",
-# 					 selex_type=c("logistic","logistic"),
-# 					 M50=34,
-# 					 M95=NULL,
-# 					 maturity_input="length",
-# 					 M=0.27, 
-# 					 binwidth=1,
-# 					 CVlen=0.1,
-# 					 SigmaR=0.737,
-# 					 SigmaF=0.2,
-# 					 SigmaC=0.2,
-# 					 SigmaI=0.2,
-# 					 R0=1,
-# 					 qcoef=1e-5,
-# 					 start_ages=0,
-# 					 rho=0.43,
-# 					 nseasons=1,
-# 					 nfleets=2)
 
 lh <- create_lh_list(vbk=0.21, 
 					 linf=65, 
@@ -55,7 +30,7 @@ lh <- create_lh_list(vbk=0.21,
 					 M95=NULL,
 					 maturity_input="length",
 					 M=0.27, 
-					 binwidth=1,
+					 binwidth=2,
 					 CVlen=0.1,
 					 SigmaR=0.2,
 					 SigmaF=0.2,
@@ -65,124 +40,70 @@ lh <- create_lh_list(vbk=0.21,
 					 qcoef=1e-5,
 					 start_ages=0,
 					 rho=0.43,
-					 nseasons=1,
-					 nfleets=1)
+					 nseasons=1)
 
-## by age
-p <- ggplot(lh$df %>% 
-			filter(Variable %in% c("Length","Weight","Selectivity","Maturity")) %>% 
-			filter(By == "Age")) + 
-	geom_line(aes(x=X, y=Value, color=Fleet), lwd=2) +
-	facet_wrap(~Variable, scale="free_y") +
-	xlab("Age") +
-	theme_lsd()
-
-## by length
-p <- ggplot(lh$df %>% 
-			filter(Variable %in% c("Selectivity","Maturity")) %>% 
-			filter(By == "Length")) + 
-	geom_line(aes(x=X, y=Value, color=Fleet), lwd=2) +
-	facet_grid(Variable~., scale="free_y") +
-	xlab("Length") +
-	theme_lsd()
-
+par(mfrow=c(2,2))
+plot(lh$L_a, type="l", lwd=3, col="forestgreen")
+plot(lh$W_a, type="l", lwd=3, col="forestgreen")
+plot(lh$Mat_a, type="l", lwd=3, col="forestgreen")
+plot(lh$S_l, type="l", lwd=3, col="forestgreen")
 
 ##----------------------------------------------------
 ## Step 2: Setup data input
 ## ---------------------------------------------------
 ## Demonstrate data generation option
 ## specify model path to save true population/generated data
-# true <- generate_data(modpath=NULL,
-# 					  itervec=1, 
-# 					  Fdynamics=c("Constant","Endogenous"),
-# 					  Rdynamics="Constant",
-# 					  lh=lh,
-# 					  Nyears=20,
-# 					  Nyears_comp=c(20,10),
-# 					  comp_sample=200,
-# 					  init_depl=0.7,
-# 					  seed=44,
-# 					  fleet_percentage=c(0.5,0.5))
 true <- generate_data(modpath=NULL,
 					  itervec=1, 
-					  Fdynamics=c("Constant"),
+					  Fdynamics="Endogenous",
 					  Rdynamics="Constant",
 					  lh=lh,
 					  Nyears=20,
-					  Nyears_comp=c(20),
+					  Nyears_comp=20,
 					  comp_sample=200,
 					  init_depl=0.7,
-					  seed=28,
-					  fleet_percentage=1)
+					  seed=28)
 
 
 ## plot simulated data
-dfsim <- true$dfsim
-p <- ggplot(dfsim %>% filter(Variable %in% c("LengthComp", "TotalBiomass", "SpawningBiomass") == FALSE)) +
-	geom_line(aes(x=X, y=Value, colour=Fleet), lwd=2) +
-	facet_wrap(~Variable, scale='free_y') +
-	expand_limits(y=0) +
-	xlab("Year") +
-	theme_lsd()
+par(mfrow=c(2,2))
+plot(true$F_t, type="l", lwd=3, col="steelblue", ylim=c(0, max(true$F_t)*1.2))
+plot(true$R_t, type="l", lwd=3, col="steelblue", ylim=c(0, max(true$R_t)*1.2))
+plot(true$SPR_t, type="l", lwd=3, col="steelblue", ylim=c(0, 1))
+plot(true$D_t, type="l", lwd=3, col="steelblue", ylim=c(0, max(true$D_t)*1.2))
+
 
 #######################################
 ## Length comp data input options
 #######################################
-## Option 1: Length comp array
-LF_array <- true$LF ## array with rows = years, columns = upper length bins, 3rd dimension = fleets
-
-## Option 2: Length comp list
-LF_list <- lapply(1:lh$nfleets, function(x) true$LF[,,x]) ##list with 1 element per fleet, and each element is a matrix with rows = years, columns = upper length bins
-
-	## plot length composition data using LF_list
-	plot_LCfits(LFlist=LF_list) ## "Inputs" argument just must be a list with "LF" as one of the components, e.g. plot_LCfits(Inputs=list("LF"=true$LF))
-
-## Option 3: Data frame
-LF_df <- true$dfsim %>% filter(Variable == "LengthComp") ## long-form data frame where "X" = year, "Value"=length measurement, and "Fleet"=discrete variables representing a fleet. 
+LF_matrix <- true$LF
 
 ## example with length data only
-data_LF <- list("years"=1:true$Nyears, "LF"=LF_array)
+data_LF <- list("years"=1:true$Nyears, "LF"=LF_matrix)
 
-##if using multinomial distribution, must specify annual effective sample size by fleet
-data_LF_neff <- list("years"=1:true$Nyears, "LF"=LF_array, "neff_ft"=true$obs_per_year)
-
-## create model inputs with life history information and data
-## outputs length data as array
-inputs_LC <- create_inputs(lh=lh, input_data=data_LF)
+## plot generated length data
+plot_LCfits(LFlist=list("LF"=LF_matrix))
 
 
 ##----------------------------------------------------
 ## Step 3: Run Model
 ## ---------------------------------------------------
-##-------------------------
-## template file
-# ##--------------------------
-src_dir <- file.path("C:\\merrill\\LIME\\src")
-setwd(src_dir)
-compile("LIME.cpp")
-
-dyn.load( dynlib("LIME") )
-
 
 ##--------------------------
 ## build inputs and object
 ##--------------------------
 
-res <- run_LIME(modpath=NULL, 
-				input=inputs_LC,
+res <- run_LIME(modpath=NULL,
+				lh=lh,  
+				input=data_LF,
 				data_avail="LC",
-				Fpen=1,
-				SigRpen=1,
-				SigRprior=c(0.737,0.3),
 				LFdist=1,
-				C_type=0,
-				est_more=FALSE,
-				fix_more=FALSE,
-				f_startval_ft=NULL,
-				rdev_startval_t=NULL,
-				est_selex_f=TRUE,
+				C_opt=0,
+				est_sigma="log_sigma_R",
+				fix_param=FALSE,
+				fix_param_t=FALSE,
 				randomR=TRUE,
-				newtonsteps=FALSE,
+				newtonsteps=3,
 				F_up=10,
 				S50_up=lh$linf,
 				derive_quants=FALSE,
@@ -222,7 +143,7 @@ Sdreport <- res$Sdreport
 ## Step 4: Plot fits
 ## ---------------------------------------------------
 ## plot length composition data
-plot_LCfits(LFlist=LF_list, 
+plot_LCfits(LFlist=list("LF"=LF_matrix), 
 			Inputs=Inputs, 
 			Report=Report)
 
