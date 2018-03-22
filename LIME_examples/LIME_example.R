49449011670671a622b76c75e422ec69b183230d
merrillrudd <merrillrudd@gmail.com>
2018-03-22 11:23:33 -0700

    update example file

--- LIME_examples/LIME_example.R
+++ LIME_examples/LIME_example.R
@@ -135,7 +135,6 @@ inputs_all <- create_inputs(lh=lh, input_data=data_all)
 ##----------------------------------------------------
 ## Step 3: Run data-rich model
 ## ---------------------------------------------------
-## dirichlet-multinomial not currently working for nfleets > 1
 rich <- run_LIME(modpath=NULL, 
 				input=inputs_all,
 				data_avail="Index_Catch_LC",
@@ -484,163 +483,10 @@ plot_output(Inputs=Inputs,
 
 
 
-src_dir <- file.path("C:\\merrill\\LIME\\src")
-setwd(src_dir)
-compile("LIME.cpp")
-dyn.load(dynlib("LIME"))
-
-##----------------------------------------------------
-## Check increasing bin width
-## ---------------------------------------------------
-## life history list
-lh <- create_lh_list(vbk=0.21, 
-					 linf=65, 
-					 t0=-0.01,
-					 lwa=0.0245, 
-					 lwb=2.79, 
-					 S50=c(20,30), 
-					 S95=c(26,36), 
-					 selex_input="length",
-					 selex_type=c("logistic","logistic"),
-					 M50=34,
-					 M95=NULL,
-					 maturity_input="length",
-					 M=0.27, 
-					 h=0.7,
-					 binwidth=2,
-					 CVlen=0.1,
-					 SigmaR=0.737,
-					 SigmaF=0.2,
-					 SigmaC=0.1,
-					 SigmaI=0.1,
-					 R0=1,
-					 qcoef=1e-5,
-					 start_ages=0,
-					 rho=0.43,
-					 nseasons=1,
-					 nfleets=2)
-
-## generate data
-true <- generate_data(modpath=NULL,
-					  itervec=1, 
-					  Fdynamics=c("Constant","Endogenous"),
-					  Rdynamics="Constant",
-					  lh=lh,
-					  Nyears=20,
-					  Nyears_comp=c(20,10),
-					  comp_sample=400,
-					  init_depl=0.7,
-					  seed=132,
-					  fleet_proportions=c(0.6,0.4))
-
-## plot simulated data
-par(mfrow=c(3,2))
-plot(true$SPR_t, type="l", ylim=c(0,1), lwd=2, xlab="Time", ylab="SPR")
-plot(true$R_t, type="l", ylim=c(0,3), lwd=2, xlab="Time", ylab="Recruitment")
-plot(x=1,y=1,type="n", ylim=c(0,1), xlim=c(1,length(true$SPR_t)), xlab="Time", ylab="Fishing mortality")
-lty <- ifelse(lh$nfleets==1,1,2)
-for(f in 1:lh$nfleets){
-	lines(true$F_ft[f,], lwd=2, lty=lty)
-}
-plot(true$D_t, type="l", ylim=c(0,2), lwd=2, xlab="Time", ylab="Relative spawning biomass")
-plot(x=1, y=1, type="n", ylim=c(0,max(true$Cw_ft)), xlim=c(1,length(true$SPR_t)), xlab="Time", ylab="Catch (biomass)")
-for(f in 1:lh$nfleets){
-	lines(true$Cw_ft[f,], lwd=2, lty=lty)
-}
-plot(x=1, y=1, type="n", ylim=c(0,max(true$I_ft)), xlim=c(1,length(true$SPR_t)), xlab="Time", ylab="Abundance index")
-for(f in 1:lh$nfleets){
-	lines(true$I_ft[f,], lwd=2, lty=lty)
-}
-
-#######################################
-## Length comp data input options
-#######################################
-## Option 1: Length comp array
-LF_array <- true$LF ## array with rows = years, columns = upper length bins, 3rd dimension = fleets
-
-## Option 2: Length comp list
-LF_list <- lapply(1:lh$nfleets, function(x) true$LF[,,x]) ##list with 1 element per fleet, and each element is a matrix with rows = years, columns = upper length bins
-
-	## plot length composition data using LF_list
-	plot_LCfits(LFlist=LF_list, ylim=c(0,0.15)) ## "Inputs" argument just must be a list with "LF" as one of the components, e.g. plot_LCfits(Inputs=list("LF"=true$LF))
-
-## Option 3: Data frame
-# LF_df <- true$dfsim %>% filter(Variable == "LengthComp") ## long-form data frame where "X" = year, "Value"=length measurement, and "Fleet"=discrete variables representing a fleet. 
-
-## example with length data only
-data_LF <- list("years"=1:true$Nyears, "LF"=LF_array)
-
-##if using multinomial distribution, must specify annual effective sample size by fleet
-data_LF_neff <- list("years"=1:true$Nyears, "LF"=LF_array, "neff_ft"=true$obs_per_year)
-
-## create model inputs with life history information and data
-## outputs length data as array
-inputs_LC <- create_inputs(lh=lh, input_data=data_LF)
-
-#######################################
-## Other data type input options
-#######################################
-data_all <- list("years"=1:true$Nyears, "LF"=LF_array, "I_ft"=true$I_ft, "C_ft"=true$Cw_ft, "neff_ft"=true$obs_per_year)
-inputs_all <- create_inputs(lh=lh, input_data=data_all)
-
-## dirichlet-multinomial not currently working for nfleets > 1
-rich <- run_LIME(modpath=NULL, 
-				input=inputs_all,
-				data_avail="Index_Catch_LC",
-				Fpen=1,
-				SigRpen=1,
-				SigRprior=c(0.737,0.3),
-				LFdist=0,
-				C_type=2,
-				est_more=FALSE,
-				fix_more=FALSE,
-				mirror=NULL,
-				f_startval_ft=NULL,
-				rdev_startval_t=NULL,
-				est_selex_f=TRUE,
-				randomR=TRUE,
-				newtonsteps=3,
-				F_up=10,
-				S50_up=lh$linf,
-				derive_quants=FALSE,
-				itervec=NULL,
-				rewrite=TRUE,
-				simulation=FALSE,
-				est_totalF=FALSE,
-				prop_f=1)
-
-## check TMB inputs
-Inputs <- rich$Inputs
-
-## Report file
-Report <- rich$Report
-
-## Standard error report
-Sdreport <- rich$Sdreport
-
-## derived reference points
-Derived <- rich$Derived
-
-
-## check convergence
-hessian <- Sdreport$pdHess
-gradient <- rich$opt$max_gradient <= 0.001
-check <- hessian == TRUE & gradient == TRUE
-
-## plot length composition data
-plot_LCfits(LFlist=LF_list, 
-			Inputs=Inputs, 
-			Report=Report,
-			ylim=c(0,0.2))
-
-## plot model output
-plot_output(Inputs=Inputs, 
-			Report=Report,
-			Sdreport=Sdreport, 
-			lh=lh,
-			True=true, 
-			plot=c("Fish","Rec","SPR","ML","SB","Selex"), 
-			set_ylim=list("SPR" = c(0,1)))
+# src_dir <- file.path("C:\\merrill\\LIME\\src")
+# setwd(src_dir)
+# compile("LIME.cpp")
+# dyn.load(dynlib("LIME"))
 
 ##----------------------------------------------------
 ## Check multiple seasons
